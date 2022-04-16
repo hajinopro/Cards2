bd5afbde27d58aea1f08f384b891d648cda0db6c
hajinopro <hajinopro@gmail.com>
2022-04-17 07:39:22 +0900

    Initial Commit

---
+++ Cards2/Cards2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cards2App.swift
+//  Cards2
+//
+//  Created by 하진호 on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct Cards2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
